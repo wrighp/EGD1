5f5a06bbdce5e0ac036334a12e7058ac96013e89
John Steigerwald <johnstiggy@gmail.com>
2018-01-23 17:39:57 -0500

    :sparkles: Creation of Tower and Building Frameworks

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour {
+
+    public int plasticCost = 0;
+    public int metalCost = 0;
+    public int peopleCost = 0;
+    public int peopleBonus = 0;
+    public float resourceGenerationTime = 0;
+    public GameObject dropResource = null;
+
+    private float chargeTime = 0f;
+    private bool isDestroyed = false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.down * Time.deltaTime * TowerManager.i.waterLevelSpeed);
+
+        if (isDestroyed) return;
+
+        chargeTime += Time.deltaTime;
+        if (chargeTime >= resourceGenerationTime ) {
+            chargeTime = 0;
+            if (dropResource != null) {
+                Instantiate(dropResource, transform);
+            }
+        }
+
+        if (transform.position.y <= TowerManager.i.transform.position.y) {
+            DisableBuilding();
+        }
+    }
+
+    public void DisableBuilding() {
+        isDestroyed = true;
+        GetComponent<SpriteRenderer>().color = new Color(.5f, .5f, .5f);
+        TowerManager.i.activeBuildings.Remove(this);
+    }
+}
---
+++ Assets/Scripts/TowerManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour {
+
+    public GameObject[] buildingTypes;
+
+    [HideInInspector]
+    public static TowerManager i = null;
+    [HideInInspector]
+    public List<Building> activeBuildings = new List<Building>();
+    public float waterLevelSpeed = .15f;
+
+    private float researchLevel = 0f;
+    private int plasticAmount = 10;
+    private int metalAmount = 10;
+    private int population = 10;
+
+	// Use this for initialization
+	void Start () {
+        if (i == null) {
+            i = this;
+        } else {
+            Destroy(this);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Alpha1)){
+            AddTower(1);
+        }
+	}
+
+    public void AddTower(int i) {
+        Vector3 pos;    
+        if (activeBuildings.Count == 0) {
+            pos = new Vector3(0, transform.position.y + 1.65f, 0);
+        } else {
+            pos = new Vector3(0, activeBuildings[activeBuildings.Count - 1].transform.position.y + 3.25f, 0);
+        }
+        GameObject go = Instantiate(buildingTypes[i], pos, Quaternion.identity);
+        activeBuildings.Add(go.GetComponent<Building>());
+
+    }
+}
