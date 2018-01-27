8065f865a61bdd00b130fb8845359d238eac4447
wrighp <wrightCoder@gmail.com>
2018-01-27 11:57:15 -0500

    :construction: Scrolling areas

---
+++ Assets/Scripts/ScreenDrag.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenDrag : MonoBehaviour {
+
+	public Transform moveTarget;
+	public Vector3 scrollSpeed = Vector3.up; //Negative for moving other direction
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void FixedUpdate(){	}
+
+	public void Scroll(){
+		print(gameObject.name);
+		moveTarget.transform.position += Time.deltaTime * scrollSpeed;
+	}
+}
