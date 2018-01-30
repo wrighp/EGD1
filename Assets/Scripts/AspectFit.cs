ab10419bd9f44032b62ef8933b1d345ebbed5c2f
wrighp <wrightCoder@gmail.com>
2018-01-30 12:43:44 -0500

    :sparkles: Aspect ratio now changes ortho size

---
+++ Assets/Scripts/AspectFit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent (typeof(Camera))]
+/// <summary>
+/// Makes camera view extend vertically instead of horizontally when aspect ratio is changed
+/// </summary>
+public class AspectFit : MonoBehaviour {
+
+	Camera camera;
+	public float baseSize = 7.7f;
+
+	// Use this for initialization
+	void Start () {
+		camera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		camera.orthographicSize = baseSize / camera.aspect;
+	}
+}
